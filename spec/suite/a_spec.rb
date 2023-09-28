af29d7a01f7e8f5b588c64d2fe899ef1913bae77
Federico Iachetti <iachetti.federico@gmail.com>
2023-09-28 18:39:36 -0300

    WIP: Adding specs

--- spec/suite/a_spec.rb
+++ spec/suite/a_spec.rb
@@ -1,24 +1,51 @@
 # frozen_string_literal: true
 
+require "awesome_print"
+AwesomePrint.defaults = {
+  indent: 2,
+  index:  false,
+}
+
 require "livetyping/tracker"
 
-RSpec.describe "Something" do
-  it "can start" do
-    tracker = Livetyping::Tracker.load
+RSpec.describe Livetyping::Tracker do
+  let(:tracker) { Livetyping::Tracker.load }
 
+  def track!(&block)
     tracker.start
-
-    expect(tracker.started?).to eq(true)
-
+    block.call
     tracker.stop
   end
 
-  it "can stop" do
-    tracker = Livetyping::Tracker.load
+  describe "starting and stopping" do
+    it "can start" do
+      tracker = Livetyping::Tracker.load
 
-    tracker.start
-    tracker.stop
+      tracker.start
 
-    expect(tracker.started?).to eq(false)
+      expect(tracker.started?).to eq(true)
+
+      tracker.stop
+    end
+
+    it "can stop" do
+      tracker = Livetyping::Tracker.load
+
+      tracker.start
+      tracker.stop
+
+      expect(tracker.started?).to eq(false)
+    end
+  end
+
+  describe "tracking" do
+    it "does something" do
+      obj = Object.new
+      track! do
+        
+      end
+
+      puts Hola
+    end
   end
 end
